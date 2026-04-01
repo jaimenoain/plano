d3c02f31372ab8f903b918db10e65cb41c2d90c1
Jaime Noain <jnoain@gmail.com>
2026-04-01 20:16:18 +0100

    Update routing configuration to include Vercel preset for enhanced SSR support
    - Added `vercelPreset` to the routing configuration in `react-router.config.ts` to improve server-side rendering capabilities and streamline deployment with Vercel.

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,10 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
   ssr: true,
   appDirectory: "src",
   routes: "app/routes.ts",
+  presets: [vercelPreset()],
 } satisfies Config;
 
